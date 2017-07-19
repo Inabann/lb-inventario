6b5ceb9cbc6db6bb7e0ca32c15860b397ceda286
inaban <25hbaam@gmail.com>
2017-07-19 15:23:01 -0500

    login terminado

---
+++ server/boot/script.js
@@ -0,0 +1,36 @@
+module.exports = function(app) {
+  let created = true; //para q no se repita otra vez, cuando ya se ha creaedo un admin
+  if (!created) {
+    var User = app.models.usuario;
+    var Role = app.models.Role;
+    var RoleMapping = app.models.RoleMapping;
+
+    User.create([
+      {username: 'admin',email: 'admin@gmail.com', password: 'admin'}
+    ], function(err, users) {
+      if (err) throw err;
+
+      console.log('Created admin:', users);
+     
+      //create the admin role
+      Role.create({
+        name: 'admin'
+      }, function(err, role) {
+        if (err) throw err;
+
+        console.log('Created role:', role);
+
+        //make an admin
+        role.principals.create({
+          principalType: RoleMapping.USER,
+          principalId: users[0].id
+        }, function(err, principal) {
+          if (err) throw err;
+          console.log('Created principal:', principal);
+        });
+      });
+    });
+  }
+
+  
+};
