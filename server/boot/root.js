e6e01db998d4e29722d8cdfcd36e12df4cd69cf7
inaban <25hbaam@gmail.com>
2017-07-28 19:39:29 -0500

    DB 2.0

--- server/boot/root.js
+++ server/boot/root.js
@@ -5,4 +5,21 @@ module.exports = function(server) {
   // var router = server.loopback.Router();
   // router.get('/', server.loopback.status());
   // server.use(router);
+  let Producto = server.models.Producto;
+  //var productoCollection = Producto.getDataSource().connector.collection(Producto.modelName);
+  //console.log(productoCollection);
+
+
+  Producto.getDataSource().connector.connect(function(err, db) {
+  let productoCollection = db.collection('Producto');
+  productoCollection.aggregate([
+    { $group: {
+      _id: "$modelosId",
+     cantidad: { $sum: "$cantidad" }
+    }}
+	  ], function(err, data) {
+	    if (err) console.log(err); //return callback(err);
+	    console.log(data); //return callback(null, data);
+	  });
+	});
 };
