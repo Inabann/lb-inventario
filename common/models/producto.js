738ec8f9a51de773dad5a91e297b25e592c0a227
inaban <25hbaam@gmail.com>
2017-08-04 01:08:59 -0500

    reducir cantidad

--- common/models/producto.js
+++ common/models/producto.js
@@ -8,7 +8,7 @@ module.exports = function(Producto) {
 	  productoCollection.aggregate([
 	    { $group: {
 	      _id: { modelo: "$modelosId", tipo: "$tiposId", marca: "$marcasId", color: "$colorsId" },
-	     cantidad: { $sum: "$cantidad" }
+	     cantidad: { $sum: "$cantidad_res" }
 	    }}
 		  ], function(err, data) {
 		    if (err) cb(err); //return callback(err);
@@ -22,6 +22,38 @@ module.exports = function(Producto) {
     returns: {arg:'data', type:['object'], root:true}
   });
 
+	Producto.reducir = function(venta, cb){
+		let porReducir = venta.cantidad;
+		Producto.find({order: 'fecha_ingreso ASC', where: { and: [{modelosId: venta.modelo}, {colorsId: venta.color}, {tiposId: venta.tipo}, {marcasId: venta.marca}] }}, 
+			function(err, data){
+				if(err) cb(err);
+				data.forEach(producto => {
+					if(producto.cantidad_res >= porReducir){//si la cantidad por reducir es menor q la cantidad restante
+						producto.cantidad_res -= porReducir;
+						Producto.upsertWithWhere({where: {id: producto.id}}, producto, (err, res) => {
+							if(err) return;
+							//ya q es menor o igual.. porReducir se vuelve 0, porq va a reducir todo de un solo producto
+							porReducir = 0;
+						})
+					} else if (producto.cantidad_res < porReducir){
+						porReducir -= producto.cantidad_res;
+						producto.cantidad_res = 0;
+						Producto.upsertWithWhere({where: {id: producto.id}}, producto, (err, res) => {
+							if(err) return;
+							return;
+						})
+					} else if(porReducir = 0){
+						return;
+					}
+				})
+				cb(null, data);
+			})
+	}
+
+	Producto.remoteMethod('reducir', {
+		accepts: {arg: 'venta', type: 'object'},
+		returns: {arg: 'data', type: ['object'], root: true}
+	})
 
 
 };
