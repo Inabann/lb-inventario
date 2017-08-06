749649b4bef59e2e9e7e061c625d4cfeb56f4059
Dazeth <dazethcn@gmail.com>
2017-08-05 20:07:08 -0500

    validation cliente.js

--- common/models/cliente.js
+++ common/models/cliente.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = function(Cliente) {
+Cliente.validatesLengthOf('dni_ruc', {min: 8});
+Cliente.validatesLengthOf('nombre', {min: 5});
+// Cliente.validatesLengthOf('email', {min: 8});
+Cliente.validatesLengthOf('telefono', {min: 6});
+
 
 };
