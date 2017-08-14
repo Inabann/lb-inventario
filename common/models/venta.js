418c6a5cff255b1b394cb59636f288808b5920ec
inaban <25hbaam@gmail.com>
2017-08-13 23:33:12 -0500

    ventas por mes 1.0

--- common/models/venta.js
+++ common/models/venta.js
@@ -1,5 +1,28 @@
 'use strict';
 
 module.exports = function(Venta) {
+	Venta.porMes = function(cb) {
+	Venta.getDataSource().connector.connect(function(err, db) {
+	  let ventaCollection = db.collection('Venta');
+	  ventaCollection.aggregate([
+	  	{ $project: { 
+        month: { $month: "$fecha_venta" },
+        year: { $year: "$fecha_venta" }
+    	}},
+	    { $group: {
+	      _id: { year: "$year", month: "$month" },
+	    	total: { $sum: 1 }
+	    }}
+		  ], function(err, data) {
+		    if (err) cb(err); //return callback(err);
+		    cb(null, data); //return callback(null, data);
+		  });
+		});
+	}
+
+	Venta.remoteMethod('porMes', {
+    //accepts: {arg: 'filter', type: 'object', description: '{"where:{...}, "groupBy": "field"}'},
+    returns: {arg:'data', type:['object'], root:true}
+  });
 
 };
