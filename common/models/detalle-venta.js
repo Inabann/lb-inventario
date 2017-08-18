950160f843e4481b3d5ea876ed80e56294c92f1e
inaban <25hbaam@gmail.com>
2017-08-18 17:12:22 -0500

    reportes 2.0

--- common/models/detalle-venta.js
+++ common/models/detalle-venta.js
@@ -1,5 +1,93 @@
 'use strict';
 
 module.exports = function(Detalleventa) {
+	//dinero vendido por mes
+	Detalleventa.VendidoporMes = function(cb){
+		Detalleventa.getDataSource().connector.connect((err, db) => {
+			let detalleVentaCollection = db.collection('DetalleVenta');
+			detalleVentaCollection.aggregate([
+				{
+		  		$sort : { fecha_venta: -1}
+		  	},
+		  	{ $project: { 
+		  		total: 1,
+	        month: { $month: "$fecha_venta" },
+	        year: { $year: "$fecha_venta" }
+	    	}},
+	    	{ $group: {
+		      _id: { year: "$year", month: "$month" },
+		    	vendido: { $sum: "$total" }
+		    }}
+			], (err, data) => {
+				if (err) cb(err);
+				let meses = [];
+		    let cantidad = [];
+		    data.forEach(venta => {
+					if(venta._id.month == 1) meses.push('Enero')
+					else if(venta._id.month == 2) meses.push('Febrero')
+					else if(venta._id.month == 3) meses.push('Marzo')
+					else if(venta._id.month == 4) meses.push('Abril')
+					else if(venta._id.month == 5) meses.push('Mayo')
+					else if(venta._id.month == 6) meses.push('Junio')
+					else if(venta._id.month == 7) meses.push('Julio')
+					else if(venta._id.month == 8) meses.push('Agosto')
+					else if(venta._id.month == 9) meses.push('Septiembre')
+					else if(venta._id.month == 10) meses.push('Octubre')
+					else if(venta._id.month == 11) meses.push('Noviembre')
+					else if(venta._id.month == 12) meses.push('Diciembre')
+					cantidad.push(venta.vendido)	
+				})
+				let newData = {};
+				newData.meses = meses;
+				newData.cantidad = cantidad;
+		    cb(null, newData);
+			})
+		})
+	}
+
+	Detalleventa.remoteMethod('VendidoporMes', {
+    //accepts: {arg: 'filter', type: 'object', description: '{"where:{...}, "groupBy": "field"}'},
+    returns: {arg:'data', type:['object'], root:true}
+  });
+
+	//cantidad vendida hoy
+	Detalleventa.totalHoy = function(cb) {
+		// Venta.getDataSource().connector.connect(function(err, db) {
+	 //  let ventaCollection = db.collection('Venta');
+	  Detalleventa.getDataSource().connector.connect((err, db) => {
+			let detalleVentaCollection = db.collection('DetalleVenta');
+			let date = new Date();
+		  let y = date.getFullYear();
+		  let m = date.getMonth() +1;
+		  // if (m<10) m = '0'+m.toString()
+		  let d = date.getDate();
+		  // let newdate = new Date(y+"-"+m+"-"+d+"T00:00:00Z");
+			detalleVentaCollection.aggregate([
+		  	{ $project: { 
+		  		total: 1,
+		  		fecha_venta: 1, 
+	        month: { $month: "$fecha_venta" },
+	        year: { $year: "$fecha_venta" }
+	    	}},
+	    	{ $match : { year : y, month: m } }
+			], (err, data) => {
+				let totalHoy = 0
+	  		data.forEach(item => {
+	  			if(item.fecha_venta.getDate()+1 == d) totalHoy += Number(item.total)
+	  		})
+	  		let newData = {}
+	  		newData.fecha = y+ "-"+m+"-"+d
+	  		newData.total = totalHoy
+	  		cb(null, newData)
+	  	})
+		})
+	}
+
+	Detalleventa.remoteMethod('totalHoy', {
+    //accepts: {arg: 'filter', type: 'object', description: '{"where:{...}, "groupBy": "field"}'},
+    returns: {arg:'data', type:['object'], root:true}
+  });
+
+
 
 };
--- common/models/venta.js
+++ common/models/venta.js
@@ -5,17 +5,41 @@ module.exports = function(Venta) {
 	Venta.getDataSource().connector.connect(function(err, db) {
 	  let ventaCollection = db.collection('Venta');
 	  ventaCollection.aggregate([
+	  	{
+	  		$sort : { fecha_venta: -1}
+	  	},
 	  	{ $project: { 
+	  		cantidad: 1,
         month: { $month: "$fecha_venta" },
         year: { $year: "$fecha_venta" }
     	}},
 	    { $group: {
 	      _id: { year: "$year", month: "$month" },
-	    	total: { $sum: 1 }
+	    	total: { $sum: "$cantidad" }
 	    }}
 		  ], function(err, data) {
 		    if (err) cb(err); //return callback(err);
-		    cb(null, data); //return callback(null, data);
+		    let meses = [];
+		    let cantidad = [];
+		    data.forEach(venta => {
+					if(venta._id.month == 1) meses.push('Enero')
+					else if(venta._id.month == 2) meses.push('Febrero')
+					else if(venta._id.month == 3) meses.push('Marzo')
+					else if(venta._id.month == 4) meses.push('Abril')
+					else if(venta._id.month == 5) meses.push('Mayo')
+					else if(venta._id.month == 6) meses.push('Junio')
+					else if(venta._id.month == 7) meses.push('Julio')
+					else if(venta._id.month == 8) meses.push('Agosto')
+					else if(venta._id.month == 9) meses.push('Septiembre')
+					else if(venta._id.month == 10) meses.push('Octubre')
+					else if(venta._id.month == 11) meses.push('Noviembre')
+					else if(venta._id.month == 12) meses.push('Diciembre')
+					cantidad.push(venta.total)	
+				})
+				let newData = {};
+				newData.meses = meses;
+				newData.cantidad = cantidad;
+		    cb(null, newData); //return callback(null, data);
 		  });
 		});
 	}
@@ -25,4 +49,49 @@ module.exports = function(Venta) {
     returns: {arg:'data', type:['object'], root:true}
   });
 
+  //producto mas vendido por mes
+  Venta.porProducto = function(cb) {
+	Venta.getDataSource().connector.connect(function(err, db) {
+	  let ventaCollection = db.collection('Venta');
+	  let d = new Date();
+	  let mes = d.getMonth() +1
+	  let y = d.getFullYear()
+	  ventaCollection.aggregate([
+	  	{ $project: { 
+	  		cantidad: 1,
+        month: { $month: "$fecha_venta" },
+        year: { $year: "$fecha_venta" },
+        modelosId: 1,
+        colorsId: 1
+    	}},
+    	{ $match : { year : y, month: mes } },
+	    { $group: {
+	      _id: { modelo: "$modelosId", color: "$colorsId"},
+	    	total: { $sum: "$cantidad" }
+	    }},
+	    {
+	  		$sort : { total: -1}
+	  	}
+		  ], function(err, data) {
+		    if (err) cb(err); //return callback(err);
+		    let labels = [];
+		    let cantidad = [];
+		    data.forEach(item => {
+					labels.push(item._id.modelo +' '+item._id.color)
+					cantidad.push(item.total)	
+				})
+				let newData = {};
+				newData.labels = labels;
+				newData.cantidad = cantidad;
+		    cb(null, newData); //return callback(null, data);
+		  });
+		});
+	}
+
+	Venta.remoteMethod('porProducto', {
+    //accepts: {arg: 'filter', type: 'object', description: '{"where:{...}, "groupBy": "field"}'},
+    returns: {arg:'data', type:['object'], root:true}
+  });
+
 };
+
